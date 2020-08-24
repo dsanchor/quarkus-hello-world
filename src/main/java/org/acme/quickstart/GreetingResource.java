34face9ced22e15b2f266e0f9feed8caf1ec2052
dsanchor <dsanchor@redhat.com>
2020-08-24 08:22:54 +0200

    Added simple env var for testing

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -8,9 +8,12 @@ import javax.ws.rs.core.MediaType;
 @Path("/hello")
 public class GreetingResource {
 
+
+    private static final String ENVIRONMENT = "ENVIRONMENT";
+
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "hello";
+        return "hello " + System.getenv(ENVIRONMENT);
     }
 }
