3da3933a440e3dda2872a133f013d728ffc41448
dsanchor <dsanchor@redhat.com>
2020-09-01 10:07:27 +0200

    Added quarkus profiles and k8s config map source

--- src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -4,7 +4,7 @@ import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
@@ -15,7 +15,7 @@ public class GreetingResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(startsWith("hello"));
+             .body(is("hello local"));
     }
 
 }
