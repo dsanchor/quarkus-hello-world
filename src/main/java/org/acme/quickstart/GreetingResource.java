c50369e107be016b9ff42fa293eb99f7b748d3ce
GTPE Student <lab-user@bastion.9b84v.internal>
2023-10-06 08:07:45 +0000

    Changed greeting message

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -19,6 +19,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return message + " " + environment + "! yuyuuuyyhhhyuuyuyTyuugtu byb yy yr demo eeenvironment with webhook!";
+        return message + " " + environment + "! hyuuyuyTyuugtu byb yy yr demo eeenvironment with webhook!";
     }
 }
